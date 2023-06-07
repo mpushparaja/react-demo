d4c8ee2b69ead22dbfe7bd2a32d1a46f6e9e35f1
Push <push@Pushs-MacBook-Air.local>
2023-06-07 18:48:27 +0530

    list group added to listGroup tsx file

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,19 @@
-function ListGroup(){
-    return <h1>List Group</h1>;
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "Germany", "Paris"];
+  // items = [];
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;

80498a7dc06fba5c9ceb7bc2f21fc2fe6adfae09
Push <push@Pushs-MacBook-Air.local>
2023-06-08 20:03:34 +0530

    props, status and events added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <ListGroup></ListGroup>;
+  let items = ["New York", "San Francisco", "Tokyo", "Germany", "Paris"];
+
+  const handlerEvent = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <ListGroup
+      items={items}
+      heading="Cities"
+      onSelectedItem={handlerEvent}
+    ></ListGroup>
+  );
 }
 
 export default App;
